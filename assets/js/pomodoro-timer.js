7bba10910edd86b0e919282daf1ab994b0333125
Asha20 <vukasinstepanovic@gmail.com>
2017-01-28 14:35:59 +0100

    Now has actual tomato counters; Added breaks

--- assets/js/pomodoro-timer.js
+++ assets/js/pomodoro-timer.js
@@ -1,31 +1,71 @@
 let pomodoroTimer = (function(global) {
-    let colors = {
+    const colors = {
         blue0: "#009B95",
         blue3: "#007873",
         red3: "#C30005"
     };
 
     let settings = {
-        workTime: 1500
+        time: {
+            work: 15,
+            shortBreak: 5,
+            longBreak: 10
+        }
     };
+
+    let currentTimer = settings.time.work;
 
     let elements = {
         workCounter: {
             init: function() {
                 this.finished = 0;
-                this.items = document.querySelectorAll(".session-item");
+                this.container = document.querySelector(".session-container");
+                this.template = document.querySelector("template.tomato");
+
+                for (let i = 0; i < 4; i++) {
+                    let item = document.importNode(this.template.content, true);
+                    this.container.appendChild(item);
+                }
+                this.paths = this.container.querySelectorAll("path");
+                this.paths.forEach(path => path.style.fill = colors.red3);
             },
 
             update: function() {
-                for (let i = 0; i < this.items.length; i++) {
-                    this.items[i].style.fill =
-                        (i < this.finished) ? colors.blue3 : colors.blue0;
+                const pathFill = [
+                    "#f88e6b",
+                    "#f42300",
+                    "#d21a00",
+                    "#445324"
+                ];
+
+                for (let i = 0; i < this.paths.length; i++) {
+                    if ((i / 4) < this.finished) {
+                        this.paths[i].style.fill = pathFill[i % 4];
+                    }
+                    else {
+                        this.paths[i].style.fill = colors.red3;
+                    }
                 }
             },
 
             endSession: function() {
-                if (this.finished < 4) {
-                    this.finished += 1;
+                // if (this.finished < 4) {
+                //     this.finished += 1;
+                // }
+                if (currentTimer === settings.time.work) {
+                    if (this.finished < 4) {
+                        this.finished += 1;
+                    }
+
+                    if (this.finished === 4) {
+                        currentTimer = settings.time.longBreak;
+                    }
+                    else {
+                        currentTimer = settings.time.shortBreak;
+                    }
+                }
+                else {
+                    currentTimer = settings.time.work;
                 }
 
                 this.update();
@@ -39,12 +79,14 @@ let pomodoroTimer = (function(global) {
                 this.size = this.element.scrollWidth;
                 this.element.width = this.size;
                 this.element.height = this.size;
+
+                this.drawResetTimer(currentTimer);
             },
 
             drawTimer: function(time, maxTime) {
-                let total = 2 * Math.PI;
-                let part = total / maxTime;
-                let center = this.size / 2;
+                const total = 2 * Math.PI;
+                const part = total / maxTime;
+                const center = this.size / 2;
 
                 this.ctx.clearRect(0, 0, this.size, this.size);
                 this.ctx.fillStyle = colors.blue0;
@@ -52,7 +94,7 @@ let pomodoroTimer = (function(global) {
                 this.ctx.arc(center, center, center, 0, total);
                 this.ctx.fill();
 
-                let lineSize = this.size * 0.2;
+                const lineSize = this.size * 0.2;
                 this.ctx.beginPath();
                 this.ctx.strokeStyle = colors.blue3;
                 this.ctx.lineWidth = lineSize;
@@ -60,14 +102,19 @@ let pomodoroTimer = (function(global) {
                              0, part * time);
                 this.ctx.stroke();
 
-                let timeLeft = maxTime - time;
-                let minutes = Math.floor(timeLeft / 60);
-                let seconds = timeLeft % 60;
+                const timeLeft = maxTime - time;
+                const minutes = Math.floor(timeLeft / 60);
+                const seconds = timeLeft % 60;
+                const pad = num => ("00" + num).slice(-2);
                 this.ctx.font = `${lineSize}px Arial`;
                 this.ctx.textAlign = "center";
                 this.ctx.fillStyle = "white";
-                this.ctx.fillText(`${minutes}:${seconds}`,
+                this.ctx.fillText(`${pad(minutes)}:${pad(seconds)}`,
                                   center, center + lineSize / 3);
+            },
+
+            drawResetTimer: function(maxTime) {
+                this.drawTimer(0, maxTime);
             }
         }
     };
@@ -76,28 +123,26 @@ let pomodoroTimer = (function(global) {
     elements.workCounter.init();
 
     let buttons = (function() {
-        let btnStart = document.querySelector(".btn-start");
-        let btnStop = document.querySelector(".btn-stop");
-        let btnPause = document.querySelector(".btn-pause");
+        const btnStart = document.querySelector(".btn-start");
+        const btnStop = document.querySelector(".btn-stop");
+        const btnPause = document.querySelector(".btn-pause");
 
-        let startTime;
-        let endTime;
-        let pauseTime;
+        let startTime, endTime, pauseTime;
         let timerID;
 
         function startTimer(endTime) {
-            let secondsLeft = parseInt((endTime - Date.now()) / 1000);
-            let secondsPassed = settings.workTime - secondsLeft;
+            const secondsLeft = parseInt((endTime - Date.now()) / 1000);
+            const secondsPassed = currentTimer - secondsLeft;
 
             if (Date.now() < endTime) {
-                elements.canvas.drawTimer(secondsPassed, settings.workTime);
+                elements.canvas.drawTimer(secondsPassed, currentTimer);
             }
             else {
                 elements.workCounter.endSession();
                 stopTimer();
                 startTime = undefined;
                 pauseTime = undefined;
-                elements.canvas.drawTimer(0, settings.workTime);
+                elements.canvas.drawResetTimer(currentTimer);
             }
         }
 
@@ -114,10 +159,10 @@ let pomodoroTimer = (function(global) {
             let timeLeft;
             if (startTime === undefined) {
                 startTime = Date.now();
-                timeLeft = settings.workTime * 1000;
+                timeLeft = currentTimer * 1000;
             }
             endTime = startTime + (pauseTime || timeLeft);
-            timerID = setInterval(startTimer.bind(null, endTime), 1000);
+            timerID = setInterval(startTimer.bind(null, endTime), 100);
 
             btnStart.disabled = true;
             btnStop.disabled = false;
@@ -128,7 +173,7 @@ let pomodoroTimer = (function(global) {
             stopTimer();
             startTime = undefined;
             pauseTime = undefined;
-            elements.canvas.drawTimer(0, settings.workTime);
+            elements.canvas.drawResetTimer(currentTimer);
         });
 
         btnPause.addEventListener("click", function(event) {
